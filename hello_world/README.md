1c0842f976acb46d9e06f991f0e03c8f41dd9549
Enkhbold Ochirsuren <boldoodd@gmail.com>
2022-06-03 21:03:56 +0200

    lvgl: output 'Hello, world!' to SSD1306 using LVGL 8.2.0+ (master)

--- hello_world/README.md
+++ hello_world/README.md
@@ -1,20 +1,72 @@
 # Hello World Example
 
-Starts a FreeRTOS task to print "Hello World".
+Starts a FreeRTOS task to print "Hello World" on the SSD1306 monochrome display using the LVGL (v8.2+) ESP32 driver.
 
-(See the README.md file in the upper level 'examples' directory for more information about examples.)
+This example is derived from an existing "Hello World" example from the ESP-IDF and a demo from the "lv_port_esp32" project.
 
-## How to use example
+## 1. How to run this example
 
-Follow detailed instructions provided specifically for this example. 
+Follow detailed instructions to set up development environment and run this example. Tested on a host system with **Linux Mint 19.3**.
 
-Select the instructions depending on Espressif chip installed on your development board:
+### 1.1. General
 
-- [ESP32 Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/stable/get-started/index.html)
-- [ESP32-S2 Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/latest/esp32s2/get-started/index.html)
+```
+$ sudo apt update && sudo apt upgrade -y
+```
 
+### 1.2. Install [Pyenv](https://realpython.com/intro-to-pyenv/#installing-pyenv)
 
-## Example folder contents
+```
+$ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+$ curl https://pyenv.run | bash
+```
+
+Load pyenv automatically by adding the following commands to your **~/.bashrc**:
+
+```
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+```
+
+### 1.3. Install ESP-IDF, [getting started](https://docs.espressif.com/projects/esp-idf/en/stable/esp32/get-started/index.html)
+
+```
+$ git clone -b v4.4.1 --recursive https://github.com/espressif/esp-idf.git esp/esp-idf-v4.4.1
+$ cd esp/esp-idf-v4.4.1/
+$ ./install.sh all
+$ . ./export.sh         # add an alias 'get_idf' to your .bashrc
+```
+
+### 1.4. Install the LVGL library ported to ESP32, [lv_port_esp32](https://github.com/lvgl/lv_port_esp32)
+
+Ensure that you're in the **~/esp/esp-idf-v4.4.1** folder. Add LVGL as submodule.
+
+```
+$ git submodule add https://github.com/lvgl/lvgl.git components/lvgl
+$ git submodule add https://github.com/lvgl/lvgl_esp32_drivers.git components/lvgl_esp32_drivers
+```
+
+### 1.5. Install Eclipse, [Eclipse IDE for C/C++ Developers installation on Ubuntu 20.04](https://linuxconfig.org/eclipse-ide-for-c-c-developers-installation-on-ubuntu-20-04)
+
+```
+$ sudo tar xvf ~/Downloads/eclipse-cpp-2021-09-R-linux-gtk-x86_64.tar.gz -C /opt/
+$ sudo ln -s /opt/eclipse/eclipse /usr/local/bin/
+```
+
+Create desktop launcher (Ubuntu) or menu launcher (Linux Mint)
+
+### 1.6. Install the ESP-IDF plug-in for Eclipse, [idf-eclipse-plugin](https://github.com/espressif/idf-eclipse-plugin)
+
+Just follow the provided instructions.
+Now you're ready to run this example from Eclipse.
+
+### 1.7. Run this example
+
+Import this example as a new project.
+Build and run this demo. Follow the configure and build instructions from the ESP-IDF [Get started](https://docs.espressif.com/projects/esp-idf/en/stable/esp32/get-started/index.html) guide.
+
+## 2. Example folder contents
 
 The project **hello_world** contains one source file in C language [hello_world_main.c](main/hello_world_main.c). The file is located in folder [main](main).
 
@@ -33,20 +85,52 @@ Below is short explanation of remaining files in the project folder.
 └── README.md                  This is the file you are currently reading
 ```
 
+The content of **hello_world_main.c** is derived mostly from a demo in [lv_port_esp32](https://github.com/lvgl/lv_port_esp32) and an another [demo](https://chowdera.com/2022/04/202204132319202760.html) with the LVGL v8.2.
+
 For more information on structure and contents of ESP-IDF projects, please refer to Section [Build System](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html) of the ESP-IDF Programming Guide.
 
-## Troubleshooting
+## 3. Troubleshooting
 
-* Program upload failure
+### 3.1. Do not add **lv_conf.h** in your source manually.
 
-    * Hardware connection is not correct: run `idf.py -p PORT monitor`, and reboot your board to see if there are any output logs.
-    * The baud rate for downloading is too high: lower your baud rate in the `menuconfig` menu, and try again.
+* LVGL supports ESP-IDF's menuconfig, so configure it with 'idf.py menuconfig' and choose 'Component config' -> 'LVGL configuration'. Refer to the ESP-IDF [Get started](https://espressif-docs.readthedocs-hosted.com/projects/esp-idf/en/stable/get-started/index.html) guide.
 
-## Technical support and feedback
+### 3.2. Run-time error with I2C Manager: E (30) lvgl_i2c: Invalid port or not configured for I2C Manager: 0
 
-Please use the following feedback channels:
+* If above run-time error occurs, then the chosen I2C port used by the display must be enabled in **sdkconfig**.
+* In 'menuconfig': 'Component config' -> 'I2C Port Settings' -> 'Enable I2C port 0' -> set SDA, SCL port pins (ie., SDA=21, SCL=22)
 
-* For technical queries, go to the [esp32.com](https://esp32.com/) forum
-* For a feature request or bug report, create a [GitHub issue](https://github.com/espressif/esp-idf/issues)
+### 3.3. LV_HOR_RES_MAX, LV_VER_RES_MAX are not set in sdkconfig since LVGL v8.x
 
-We will get back to you as soon as possible.
+* Define the display specific resolution in your source. Refer this [post](https://forum.lvgl.io/t/lv-hor-res-max-and-lv-ver-res-max/5817/2) for some hints.
+ 
+### 3.4. Program upload failure
+
+* Hardware connection is not correct: run `idf.py -p PORT monitor`, and reboot your board to see if there are any output logs.
+* The baud rate for downloading is too high: lower your baud rate in the `menuconfig` menu, and try again.
+
+## 4. (Optional) Run a demo provided in LVGL port to ESP32, [lv_port_esp32](https://github.com/lvgl/lv_port_esp32)
+
+Download 'lv_port_esp32' example from Git repo.
+Replace default ILI9341 with SSD1306 (changes sdkconfig).
+Compilation error can be fixed by editing ssd1306.c file.
+
+### 4.1. Install binary image to ESP32
+
+Assume the ESP32 board is connected as '/dev/ttyUSB0'.
+To flash the binary image, invoke a following command:
+
+```
+$ idf.py -p /dev/ttyUSB0 flash
+```
+
+### 4.2. Monitor the application
+
+The 'IDF Monitor' application is used to see the debug output message:
+
+```
+$ idf.py -p /dev/ttyUSB0 monitor
+```
+
+Both flashing and monitoring commands can be combined:
+$ idf.py -p /dev/ttyUSB0 flash monitor.
--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -1,11 +1,12 @@
-/* Hello World Example
+/* Hello World with LVGL and SSD1306.
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   This exampled code is in the Public Domain (or CC0 licensed, at your option.)
 
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
@@ -13,10 +14,73 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "esp_log.h"
+/* FreeRTOS specific */
+#ifdef CONFIG_FREEROTS_UNICORE
+
+static const BaseType_t app_cpu = 0;
+
+#else
+
+static const BaseType_t app_cpu = 1;
+
+#endif
+
+/* LVLG relevant */
+#include "freertos/semphr.h"
+#include "esp_freertos_hooks.h"
+#include "lvgl_helpers.h"
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+
+#include "lvgl.h"
+#include "demos/lv_demos.h"
+
+#else
+
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+
+#endif
+
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    #if defined CONFIG_LV_USE_DEMO_WIDGETS
+        #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
+    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+        #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
+    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+        #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
+    #elif defined CONFIG_LV_USE_DEMO_STRESS
+        #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
+    #else
+        #error "No demo application selected."
+    #endif
+#endif
+
+/* Defines */
+#define TAG "lvgl_demo"
+#define LV_TICK_PERIOD_MS 1
+
+/* Custom defines for SSD1306 resolution
+ *
+ * LV_HOR_RES_MAX and LV_VER_RES_MAX are not set in sdkconfig since LVGL v8.0
+ */
+#define SSD1306_HOR_RES_MAX 128
+#define SSD1306_VER_RES_MAX 64
+
+/* Function prototypes */
+static void lv_tick_task(void *arg);
+static void gui_task(void *pvParameter);
+static void create_demo(void);
+
+/* A semaphore is used to handle concurrent calls to
+ * lv_* functions, which are not thread-safe.
+ */
+
+SemaphoreHandle_t xGuiSemaphore;
+
 void app_main(void)
 {
-    printf("Hello world!\n");
-
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
@@ -33,11 +97,144 @@ void app_main(void)
 
     printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    xTaskCreatePinnedToCore(gui_task,
+		"gui",
+		4096 *2,
+		NULL,
+		0,
+		NULL,
+		app_cpu);
+}
+
+static void lv_tick_task(void *arg)
+{
+	(void) arg;
+	lv_tick_inc(LV_TICK_PERIOD_MS);
+}
+
+static void gui_task(void *pvParameter)
+{
+	(void) pvParameter;
+	xGuiSemaphore = xSemaphoreCreateMutex();
+
+	// Initialize LVGL
+	lv_init();
+
+	// Initialize the SPI or I2C bus used by the drivers
+	lvgl_driver_init();
+
+	// Initialize the frame buffers
+	lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+	assert(buf1 != NULL);
+
+	// Use double buffers if not working with monochrome displays
+	lv_color_t* buf2 = NULL;
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+	buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+	assert(buf2 != NULL);
+#endif
+
+	static lv_disp_draw_buf_t disp_buf;
+	uint32_t size_in_px = DISP_BUF_SIZE;
+
+#if defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_IL3820 \
+	    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_JD79653A \
+	    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_UC8151D \
+	    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_SSD1306
+	// actual size in pixels, not bytes
+	size_in_px *= 8;
+#endif
+
+	// Initialize the working buffer depending on the selected display.
+	// Note: buf2 == NULL, if monochrome display is used
+	lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
+
+	// Initialize the display driver
+	lv_disp_drv_t disp_drv;
+	lv_disp_drv_init(&disp_drv);
+	disp_drv.flush_cb = disp_driver_flush;  // callback to flush frame buffers
+
+	// When using a monochrome display, register the callbacks
+#ifdef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+	disp_drv.rounder_cb = disp_driver_rounder;
+	disp_drv.set_px_cb = disp_driver_set_px;
+#endif
+
+	disp_drv.draw_buf = &disp_buf;
+	disp_drv.hor_res = SSD1306_HOR_RES_MAX;
+	disp_drv.ver_res = SSD1306_VER_RES_MAX;
+	lv_disp_drv_register(&disp_drv);
+
+	// Create and start a timer to call lv_tick_inc()
+	const esp_timer_create_args_t disp_timer_args = {
+			.callback = &lv_tick_task,
+			.name = "periodic_gui",
+	};
+
+	esp_timer_handle_t disp_timer;
+	ESP_ERROR_CHECK(esp_timer_create(&disp_timer_args, &disp_timer));
+	ESP_ERROR_CHECK(esp_timer_start_periodic(disp_timer, LV_TICK_PERIOD_MS * 1000));
+
+	// Create a demo application
+	create_demo();
+
+	// Task loop
+	while(1) {
+		// Delay 1 tick (assume FreeRTOS has 10 ms tick by default)
+		vTaskDelay(pdMS_TO_TICKS(10));
+
+		// Try to take the semaphore, on success call lv_* function
+		if (xSemaphoreTake(xGuiSemaphore, portMAX_DELAY) == pdTRUE) {
+			lv_task_handler();
+
+			// Give the semaphore
+			xSemaphoreGive(xGuiSemaphore);
+		}
+	}
+
+	// This task should NEVER run here
+	free(buf1);
+
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+	free(buf2);
+#endif
+	vTaskDelete(NULL);
+}
+
+static void create_demo(void)
+{
+	// When using monochrome display, display "Hello, world!" on the center of the display.
+#if defined CONFIG_LV_TFT_DISPLAY_MONOCHROME || \
+    defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_ST7735S
+
+    // Get the current screen
+    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
+
+    // Create a Label on the currently active screen
+    lv_obj_t * label1 =  lv_label_create(scr);
+
+    // Modify the Label's text
+    lv_label_set_text(label1, "Hello\nworld!");
+
+    // Align the Label to the center
+    // NULL means align on parent (which is the screen now)
+    // 0, 0 at the end means an x, y offset after alignment
+    lv_obj_align(label1, LV_ALIGN_CENTER, 0, 0);
+#else
+    // Otherwise we show the selected demo
+
+    #if defined CONFIG_LV_USE_DEMO_WIDGETS
+        lv_demo_widgets();
+    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+        lv_demo_keypad_encoder();
+    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+        lv_demo_benchmark();
+    #elif defined CONFIG_LV_USE_DEMO_STRESS
+        lv_demo_stress();
+    #else
+        #error "No demo application selected."
+    #endif
+#endif
+    // debug output
+    ESP_LOGI(TAG, "created demo app");
 }
