36e883ea6cdfd6960bfca1a3e88a4d9ff1f63ed5
Enkhbold Ochirsuren <boldoodd@gmail.com>
2022-06-11 22:04:20 +0200

    display the current system time on screen

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -13,6 +13,15 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_event.h"
+#include "time.h"
+
+/* Define system time base event */
+ESP_EVENT_DEFINE_BASE(ESP_SYSTEM_TIME_EVENT);
+
+typedef enum {
+	SYSTEM_TIME_UPDATE
+} sys_time_evt_id_t;
 
 #include "esp_log.h"
 /* FreeRTOS specific */
@@ -68,16 +77,28 @@ static const BaseType_t app_cpu = 1;
 #define SSD1306_HOR_RES_MAX 128
 #define SSD1306_VER_RES_MAX 64
 
+lv_obj_t *screen_label;
+
 /* Function prototypes */
 static void lv_tick_task(void *arg);
-static void gui_task(void *pvParameter);
-static void create_demo(void);
+static void display_task(void *pvParameter);
+static lv_obj_t* create_screen_label(lv_disp_t* screen);
+static void display_text(lv_obj_t* label, char *disp_text);
+static void sys_time_evt_handler(void* handler_arg, esp_event_base_t base, int32_t id, void* data);
 
 /* A semaphore is used to handle concurrent calls to
  * lv_* functions, which are not thread-safe.
  */
 
 SemaphoreHandle_t xGuiSemaphore;
+
+/*
+ * System time
+ */
+
+time_t now;
+char strftime_buf[64];  // store formatted timeinfo
+struct tm timeinfo;
 
 void app_main(void)
 {
@@ -97,8 +118,19 @@ void app_main(void)
 
     printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
-    xTaskCreatePinnedToCore(gui_task,
-		"gui",
+    /* Get the system time and output to log */
+    time(&now);
+    setenv("TZ", "CEST+2", 1); // Central European Summer Time
+    tzset();
+
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    printf("The current system time: %s\n", strftime_buf);
+
+    ESP_LOGI(TAG, "current system time: %s\n", strftime_buf);
+
+    xTaskCreatePinnedToCore(display_task,
+		"display_task",
 		4096 *2,
 		NULL,
 		0,
@@ -112,7 +144,7 @@ static void lv_tick_task(void *arg)
 	lv_tick_inc(LV_TICK_PERIOD_MS);
 }
 
-static void gui_task(void *pvParameter)
+static void display_task(void *pvParameter)
 {
 	(void) pvParameter;
 	xGuiSemaphore = xSemaphoreCreateMutex();
@@ -168,15 +200,35 @@ static void gui_task(void *pvParameter)
 	// Create and start a timer to call lv_tick_inc()
 	const esp_timer_create_args_t disp_timer_args = {
 			.callback = &lv_tick_task,
-			.name = "periodic_gui",
+			.name = "disp_timer",
 	};
 
 	esp_timer_handle_t disp_timer;
 	ESP_ERROR_CHECK(esp_timer_create(&disp_timer_args, &disp_timer));
 	ESP_ERROR_CHECK(esp_timer_start_periodic(disp_timer, LV_TICK_PERIOD_MS * 1000));
 
-	// Create a demo application
-	create_demo();
+	// Create a screen label
+	screen_label = create_screen_label(NULL);
+	assert(screen_label != NULL);
+
+	// Display a given text on screen
+	display_text(screen_label, "Good morning!");
+
+	/* Create an event loop */
+	esp_event_loop_args_t loop_args = {
+			.queue_size = 2,
+			.task_name = NULL,
+			.task_priority = 0,
+			.task_stack_size = 2048,
+			.task_core_id = app_cpu
+	};
+
+	esp_event_loop_handle_t loop_handle;
+	esp_event_loop_create(&loop_args, &loop_handle);
+
+	esp_event_handler_register_with(loop_handle,
+			ESP_SYSTEM_TIME_EVENT, ESP_EVENT_ANY_ID,
+			sys_time_evt_handler, NULL);
 
 	// Task loop
 	while(1) {
@@ -190,6 +242,11 @@ static void gui_task(void *pvParameter)
 			// Give the semaphore
 			xSemaphoreGive(xGuiSemaphore);
 		}
+
+		esp_event_post_to(loop_handle, ESP_SYSTEM_TIME_EVENT, SYSTEM_TIME_UPDATE, NULL, 0, pdMS_TO_TICKS(1000));
+
+		esp_event_loop_run(loop_handle, pdMS_TO_TICKS(1000));
+
 	}
 
 	// This task should NEVER run here
@@ -201,40 +258,54 @@ static void gui_task(void *pvParameter)
 	vTaskDelete(NULL);
 }
 
-static void create_demo(void)
+static lv_obj_t* create_screen_label(lv_disp_t* screen)
 {
-	// When using monochrome display, display "Hello, world!" on the center of the display.
+	lv_obj_t* label;
+
+	// Monochrome display
 #if defined CONFIG_LV_TFT_DISPLAY_MONOCHROME || \
     defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_ST7735S
 
     // Get the current screen
-    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
+    lv_obj_t * scr = lv_disp_get_scr_act(screen);
 
     // Create a Label on the currently active screen
-    lv_obj_t * label1 =  lv_label_create(scr);
+    label =  lv_label_create(scr);
 
     // Modify the Label's text
-    lv_label_set_text(label1, "Hello\nworld!");
+    //lv_label_set_long_mode(label1, LV_LABEL_LONG_SCROLL_CIRCULAR); // circular scrolling
+	//lv_obj_set_width(label1, 96);
+    //lv_label_set_text(label1, disp_text);
 
     // Align the Label to the center
     // NULL means align on parent (which is the screen now)
     // 0, 0 at the end means an x, y offset after alignment
-    lv_obj_align(label1, LV_ALIGN_CENTER, 0, 0);
-#else
-    // Otherwise we show the selected demo
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        lv_demo_widgets();
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        lv_demo_keypad_encoder();
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        lv_demo_benchmark();
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        lv_demo_stress();
-    #else
-        #error "No demo application selected."
-    #endif
 #endif
     // debug output
-    ESP_LOGI(TAG, "created demo app");
+    ESP_LOGI(TAG, "created a label");
+
+    return label;
+}
+
+static void display_text(lv_obj_t* label, char *disp_text)
+{
+    // Modify the Label's text
+    lv_label_set_text(label, disp_text);
+    ESP_LOGI(TAG, "display: %s\n", disp_text);
+}
+
+static void sys_time_evt_handler(void* handler_arg, esp_event_base_t base, int32_t id, void* data)
+{
+	time(&now);
+	localtime_r(&now, &timeinfo);
+	// %d: day of month (01-31)
+	// %m: month as decimal number (01-12)
+	// %Y: year
+	// %X : time representation
+	strftime(strftime_buf, sizeof(strftime_buf), "%d.%m.%Y\n%X", &timeinfo);
+
+	ESP_LOGI(TAG, "time_evt_handler");
+	display_text(screen_label, strftime_buf);
 }
